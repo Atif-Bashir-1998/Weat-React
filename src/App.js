b1482adba96dd36ea92c770e640214041857b320
Atif-Bashir-1998 <adobe8832289@gmail.com>
2022-03-11 11:53:33 +0500

    tailwind CSS added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Error from "./views/Error";
+import './index.css';
 
 import Home from './views/Home'
 

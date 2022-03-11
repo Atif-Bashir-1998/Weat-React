052abcd7efa756748f7e50e957043879c8286708
Atif-Bashir-1998 <adobe8832289@gmail.com>
2022-03-11 21:27:20 +0500

    Homepage completed

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,6 +2,34 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div className='bg-red-300'>Home</div>
+    <div className='p-5'>
+      <section className='mb-5'>
+        <h1 className='font-bold underline text-2xl'>What is Weat-React?</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+          molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+          numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+          optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+          obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+          nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+          tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+          quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
+          sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
+          recusandae alias error harum maxime adipisci amet laborum.</p>
+      </section>
+
+      <section>
+        <h1 className='font-bold underline text-2xl'>Technologies used</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+          molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+          numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+          optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+          obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+          nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+          tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+          quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
+          sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
+          recusandae alias error harum maxime adipisci amet laborum.</p>
+      </section>
+    </div>
   )
 }

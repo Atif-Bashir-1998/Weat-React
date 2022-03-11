8b372077276f66d0a0304721b6b29589bbd57415
Atif-Bashir-1998 <adobe8832289@gmail.com>
2022-03-11 11:49:00 +0500

    page not found error page added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react"
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Error from "./views/Error";
 
 import Home from './views/Home'
 
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </Router>
   );

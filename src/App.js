48821c61f6311da9a70e7a090f2303becaca95ea
Atif-Bashir-1998 <adobe8832289@gmail.com>
2022-03-11 21:09:20 +0500

    react router added along with navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from "react"
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
 import Error from "./views/Error";
 import './index.css';
 
 import Home from './views/Home'
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 

555743a0c8b12679b980d1c28f0cf666defb49e8
Atif-Bashir-1998 <adobe8832289@gmail.com>
2022-04-10 22:33:53 +0500

    redux store is available to the app

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'font-awesome/css/font-awesome.min.css'
 
+// importing the store
+import store from "./state/store";
+
+import {Provider} from 'react-redux';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );

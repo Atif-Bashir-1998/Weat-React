788cc5942bd4e32fd1ce2b4522f3a803de22b1fe
Atif-Bashir-1998 <adobe8832289@gmail.com>
2022-04-11 19:38:56 +0500

    astronomy info is fetched from api

--- src/api/WeatherAPI.js
+++ src/api/WeatherAPI.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
+import dayjs from 'dayjs'
 const apiKey = process.env.REACT_APP_API_KEY
+const today = dayjs().format('YYYY-MM-DD')
 
 const getWeather = async (location) => {
     try {
@@ -10,4 +12,13 @@ const getWeather = async (location) => {
     }
 }
 
-export default getWeather
+const getAstronomy = async (location) => {
+    try {
+        const response = await axios.get(`http://api.weatherapi.com/v1/astronomy.json?key=${apiKey}&q=${location}&dt=${today}`)
+        return {data: response.data}
+    } catch (error) {
+        return {error}
+    }
+}
+
+export {getWeather, getAstronomy}

e59243631cb9e499e8068c5312da889bcfe627f8
Atif-Bashir-1998 <adobe8832289@gmail.com>
2022-04-11 07:20:21 +0500

    location input is updating the store

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import bg from "./../images/night-clear.jpg";
 import clearNight from "./../images/clear-night.svg"
 import { useSelector, useDispatch } from "react-redux";
@@ -8,11 +8,13 @@ import { actionCreators } from "../state/index";
 export default function HeroSection() {
 	const location = useSelector((state) => state.weather.location)
 	const dispatch = useDispatch()
+	const [locationInput, setLocationInput] = useState('')
 
 	const {setLocation} = bindActionCreators(actionCreators, dispatch)
 
 	const updateLocation = () => {
-		setLocation('New Location')
+		setLocation(locationInput)
+		setLocationInput('')
 	}
 	// console.log("set: ", setLocation)
 
@@ -35,7 +37,7 @@ export default function HeroSection() {
 				<div className="flex justify-center">
 					<div className="mb-3 xl:w-96">
 						<div className="relative flex items-stretch w-full mb-4">
-							<input type="search" className="form-control relative flex-auto w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white  border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Search" />
+							<input type="search" className="form-control relative flex-auto w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white  border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Search" value={locationInput} onChange={(e) => setLocationInput(e.target.value)} />
 							<button className="px-6 py-2 border-2 border-gray-600 text-white-600 bg-indigo-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-25 focus:outline-none focus:ring-0 transition duration-150 ease-in-out" type="button" id="button-addon3" onClick={updateLocation}>Search</button>
 						</div>
 					</div>

92e9386afc936f5b43254d3e8fc10b06f32cc5b4
Atif-Bashir-1998 <adobe8832289@gmail.com>
2022-04-11 10:39:20 +0500

    changed class to className

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Alert({children}) {
   return (
     <div
-      class="bg-indigo-100 rounded-lg py-2 px-6 text-base text-indigo-600 font-bold absolute right-12 top-4"
+      className="bg-indigo-100 rounded-lg py-2 px-6 text-base text-indigo-600 font-bold absolute right-12 top-4"
       role="alert"
     >
       {children}

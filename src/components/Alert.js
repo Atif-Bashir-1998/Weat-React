9c6cb3dc29e4f098b48cebd9e680c8b930b7a84d
Atif-Bashir-1998 <adobe8832289@gmail.com>
2022-04-11 09:47:23 +0500

    Alert component added

---
+++ src/components/Alert.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Alert({children}) {
+  return (
+    <div
+      class="bg-indigo-100 rounded-lg py-2 px-6 text-base text-indigo-600 font-bold absolute right-12 top-4"
+      role="alert"
+    >
+      {children}
+    </div>
+  );
+}

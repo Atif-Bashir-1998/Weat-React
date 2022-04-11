32b89ad3dfa096c8093e126df2a82bbab0ae7a20
Atif-Bashir-1998 <adobe8832289@gmail.com>
2022-04-11 10:39:55 +0500

    weather report is being saved

--- src/state/reducers/weatherReducer.js
+++ src/state/reducers/weatherReducer.js
@@ -1,5 +1,6 @@
 const initialState = {
-    location: "Attock City, Punjab, Pakistan"
+    location: "Attock City, Punjab, Pakistan",
+    weather: {}
 }
 
 const reducer = (state = initialState, { type, payload }) => {
@@ -8,6 +9,9 @@ const reducer = (state = initialState, { type, payload }) => {
 
   case "SET_LOCATION":
     return { ...state, location: payload }
+
+  case "SET_WEATHER":
+    return {...state, weather: payload}
 
   default:
     return state

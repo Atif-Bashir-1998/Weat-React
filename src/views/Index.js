e59b8c26323350673e3ea7be6e3e0a43a4e5dc3c
Atif-Bashir-1998 <adobe8832289@gmail.com>
2022-05-05 12:14:41 +0500

    Index page layout updated

--- src/views/Index.js
+++ src/views/Index.js
@@ -3,16 +3,27 @@ import HeroSection from "../components/HeroSection";
 import HourlyForecast from "../components/HourlyForecast";
 import Recent from "../components/Recent";
 import Today from "../components/Today";
+import { useSelector } from "react-redux";
+import NoInternet from "../components/NoInternet";
 
 export default function Index() {
-  return <>
-		<HeroSection />
-		<div className="my-10">
-			<Recent />
-			<Today />
-		</div>
-		<div className="my-10">
-			<HourlyForecast />
-		</div>
-	</>;
+  const isOnline = useSelector((state) => state.online.online);
+
+  return (
+    <>
+      <HeroSection />
+      <div className="my-10">
+        <Recent />
+      </div>
+      <div>
+        {isOnline && (
+          <section className="space-y-10">
+            <Today />
+            <HourlyForecast />
+          </section>
+        )}
+        {!isOnline && <NoInternet />}
+      </div>
+    </>
+  );
 }

a594c0a0528fdf6f174a7f01ab74a69035ecb306
Atif-Bashir-1998 <adobe8832289@gmail.com>
2022-05-05 12:10:28 +0500

    online status handled by store

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -1,8 +1,10 @@
 import weatherReducer from "./weatherReducer";
+import onlineReducer from "./onlineReducer";
 import { combineReducers } from "redux";
 
 const reducers = combineReducers({
-    weather: weatherReducer
+    weather: weatherReducer,
+    online: onlineReducer
 })
 
 export default reducers

091f0177be6c7bac020cb41cc4ab6b16df1a4d93
Atif-Bashir-1998 <adobe8832289@gmail.com>
2022-05-08 06:50:02 +0500

    error page ready

--- src/views/Error.js
+++ src/views/Error.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import errorImage from './../images/404-bg.jpg'
 
 export default function Error() {
   return (
-    <div>Error Page</div>
+    <div style={{ backgroundImage: `url(${errorImage})`, height: '100vh', backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}>
+      <div className='flex flex-col items-center justify-center gap-4 h-full bg-black bg-opacity-70'>
+        <h1 className='text-purple-600 font-extrabold text-7xl text-center'>404 Page not found</h1>
+        <Link to='/' className='px-6 py-3 text-3xl bg-purple-600 text-white rounded-md'>Home</Link>
+      </div>
+    </div>
   )
 }
